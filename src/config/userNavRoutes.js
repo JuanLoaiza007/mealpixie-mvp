7ea88b38c379da24d4886f67c72854c64582b922
juanloaiza007 <juandaloaiza723@gmail.com>
2025-05-10 00:24:20 -0500

    feat: add userProfile Schemas

---
+++ src/config/userNavRoutes.js
@@ -0,0 +1,22 @@
+import { Camera, UserCircle, HomeIcon } from "lucide-react";
+
+export const userNavRoutes = [
+  {
+    id: "vision",
+    label: "Visión",
+    href: "/tools/vision",
+    icon: Camera,
+  },
+  {
+    id: "profile",
+    label: "Perfil",
+    href: "/tools/profile",
+    icon: UserCircle,
+  },
+  {
+    id: "home",
+    label: "Inicio",
+    href: "/",
+    icon: HomeIcon,
+  },
+];
