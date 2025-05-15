155ef446aff3172ec68c44b1de903ce46b75433a
juanloaiza007 <juandaloaiza723@gmail.com>
2025-05-14 21:38:07 -0500

    feat: create default gemini configuration (common)

---
+++ src/config/gemini/common.js
@@ -0,0 +1,10 @@
+export const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+
+export const DEFAULT_MODEL_NAME = "gemini-2.0-flash";
+
+export const DEFAULT_GENERATION_CONFIG = {
+  temperature: 0.2,
+  topP: 1.0,
+  topK: 40,
+  maxOutputTokens: 1024,
+};
