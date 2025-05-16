f4a889e0b31f3a6b35688ccba7b83625a97b7482
juanloaiza007 <juandaloaiza723@gmail.com>
2025-05-16 10:09:02 -0500

    fix: add validations to profiles and fix numeric fields with persistent "0"

--- src/components/schemas/userProfile.js
+++ src/components/schemas/userProfile.js
@@ -17,32 +17,26 @@ export const activityLevelOptions = [
 ];
 export const goalOptions = [
   { value: "lose", label: "Perder peso" },
-  { value: "maintain", label: "Mantenerse en forma" },
+  { value: "maintain", label: "Mantener peso" },
   { value: "gain", label: "Ganar peso" },
 ];
 
 export const userProfileSchema = z.object({
-  id: z.number().optional(), // Añade el id como opcional
+  id: z.number().optional(),
   username: z
     .string()
     .min(1, "El nombre de usuario no puede estar vacío")
     .max(30, "El nombre de usuario debe tener como máximo 30 caracteres"),
-  birthYear: z
-    .number({ invalid_type_error: "El año de nacimiento debe ser un número" })
-    .int("El año de nacimiento debe ser un entero")
-    .gte(1900, "El año de nacimiento debe ser >= 1900")
-    .lte(
-      new Date().getFullYear(),
-      `El año de nacimiento debe ser <= ${new Date().getFullYear()}`
-    ),
   height: z
     .number({ invalid_type_error: "La altura debe ser un número" })
     .positive("La altura debe ser un valor positivo")
+    .min(50, "La altura no puede ser menor a 50 cm")
     .max(300, "La altura no puede superar los 300 cm"),
   weight: z
     .number({ invalid_type_error: "El peso debe ser un número" })
     .positive("El peso debe ser un valor positivo")
-    .max(500, "El peso no puede superar los 500 kg"),
+    .min(20, "El peso no puede ser menor a 20 kg")
+    .max(300, "El peso no puede superar los 300 kg"),
   activityLevel: z.enum(activityLevels, {
     errorMap: () => ({ message: "Nivel de actividad inválido" }),
   }),
@@ -53,7 +47,6 @@ export const userProfileSchema = z.object({
 
 export const initialProfile = {
   username: "guest",
-  birthYear: 2000,
   height: 170,
   weight: 65,
   activityLevel: "moderately_active",
